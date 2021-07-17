cf1d9583ed8876067f8a77183801a047d42e70e2
Sharwari Nemane <sharwarinemane1997@gmail.com>
2021-07-17 14:52:57 +0530

    Sprint 2 commit

--- Phase1_Assessment/src/Application.java
+++ Phase1_Assessment/src/Application.java
@@ -1,18 +1,19 @@
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Application {
-
-	public static void main(String[] args) {
+	
+	public static void main(String[] args) throws IOException, InterruptedException 
+	{
 		// TODO Auto-generated method stub
 		System.out.println("LOCKEDME.COM");
 		System.out.println("Phase 1:Implement Object Oriented Programming Using JAVA With Data Structures And Beyond");
 		System.out.println("Application Name:Virtual Key For Your Repositories");
 		System.out.println("Developer:Sharwari Nemane");
 		appMenu();
-		
-		
 	}
-	public static void appMenu()
+	
+	public static void appMenu() throws IOException, InterruptedException
 	{
 		Scanner scanMenu=new Scanner(System.in);
 		String menuChoice;
@@ -21,7 +22,7 @@ public class Application {
 			System.out.println("_________________________________________________________________________________________");
 			System.out.println("*Application Menu*");
 			System.out.println("1.Retrieve file names in ascending order.");
-			System.out.println("2.Business level operations(add/delete/search).");
+			System.out.println("2.Business level operations(add/write/read/delete/search).");
 			System.out.println("3.Close the application.");
 			System.out.println("Enter your choice:");
 			menuChoice=scanMenu.nextLine();
@@ -39,38 +40,58 @@ public class Application {
 			}
 		}
 	}
+	
 	public static void getAllFiles() 
 	{
 		System.out.println("*Retrieve file names in ascending order*");
 		FileOperations fo=new FileOperations();
 		fo.getAllFiles();
 	}
-	public static void fileOperations()
+	
+	public static void fileOperations() throws IOException, InterruptedException
 	{
 		Scanner scanFO=new Scanner(System.in);
 		String fOChoice;
 		while(true)
 		{
+			System.out.println("__________________________________");
 			System.out.println("*Business level operations*");
 			System.out.println("1.Add a File.");
-			System.out.println("2.Delete a File.");
-			System.out.println("3.Search a File.");
-			System.out.println("4.Return to Application Menu.");
+			System.out.println("2.Write in a File.");
+			System.out.println("3.Read from a File.");
+			System.out.println("4.Delete a File.");
+			System.out.println("5.Search a File.");
+			System.out.println("6.Return to Application Menu.");
 			System.out.println("Enter your choice:");
 			fOChoice=scanFO.nextLine();
 			switch(fOChoice)
 			{
-				case "1":System.out.println("Add");
+				case "1":System.out.println("*Add a File*");
+					FileOperations add=new FileOperations();
+					add.addNewFile();
 					break;
-				case "2":System.out.println("Delete");
+				case "2":System.out.println("*Write in a File*");
+					FileOperations write=new FileOperations();
+					write.writeInFile();
 					break;
-				case "3":System.out.println("Search");
+				case "3":System.out.println("*Read from a File*");
+					FileOperations read=new FileOperations();
+					read.readFromFile();
 					break;
-				case "4":System.out.println("Returning to Application Menu...");
+				case "4":System.out.println("*Delete a File*");
+					FileOperations del=new FileOperations();
+					del.deleteFile();
+					break;
+				case "5":System.out.println("*Search a File*");
+					FileOperations sear=new FileOperations();
+					sear.searchFile();
+					break;
+				case "6":System.out.println("Returning to Application Menu...");
 					return;
-				default:System.out.println("Enter valid choice");
+				default:System.out.println("Enter valid choice.");
 					break;
 			}	
 		}
 	}
+	
 }
--- Phase1_Assessment/src/FileOperations.java
+++ Phase1_Assessment/src/FileOperations.java
@@ -1,21 +1,199 @@
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Scanner;
 
-public class FileOperations {
+public class FileOperations 
+{
+	
 	public void getAllFiles()
 	{
 		Scanner scanFile=new Scanner(System.in); 
 		System.out.println("Enter path of Directory:");
 		String dirPath=scanFile.nextLine();
 		File dir=new File(dirPath);
-		File[] filesList=dir.listFiles();
-		if(filesList.length!=0)
+		if(!dir.exists())
 		{
-			System.out.println("Files present in path:"+dirPath);
-			for(int i=0;i<filesList.length;i++)
-				System.out.println(filesList[i].getName());
+			System.out.println("No such Directory exists!");
+			return;
 		}
 		else
-			System.out.println("No Files present in path:"+dirPath);
+		{	File[] filesList=dir.listFiles();
+			if(filesList.length!=0)
+			{
+				System.out.println("Files present in path:"+dirPath);
+				for(int i=0;i<filesList.length;i++)
+					System.out.println(filesList[i].getName());
+			}
+			else
+			{
+				System.out.println("No Files present in path:"+dirPath);
+			}
+		}
+	}	
+	
+	public void addNewFile() throws IOException
+	{
+		Scanner scanAdd=new Scanner(System.in);
+		System.out.println("Enter the path where you want to create new File:");
+		String dirPath=scanAdd.nextLine();
+		File dir=new File(dirPath);
+		if(!dir.exists())
+		{
+			System.out.println("No such Directory exists!");
+			return;
+		}
+		else
+		{
+			System.out.println("Enter File Name to be created:");
+			String fileName=scanAdd.nextLine();
+			File file=new File(dirPath,fileName);
+			if(file.createNewFile())	
+			{
+				System.out.println("New File has been created");
+			}
+			else
+			{
+				System.out.println("Could not create new file");
+			}
+		}
 	}
+	
+	public void writeInFile() throws IOException, InterruptedException
+	{
+		Scanner scanWrite=new Scanner(System.in);
+		System.out.println("Enter the path from where you want to write in a File:");
+		String dirPath=scanWrite.nextLine(); 
+		File dir=new File(dirPath);
+		if(!dir.exists()) 
+		{
+			System.out.println("No such Directory exists!"); 
+			return;
+		} 
+		else 
+		{ 
+			System.out.println("Enter name of File to write in:"); 
+			String fileName=scanWrite.nextLine(); 
+			File file=new File(dirPath,fileName);
+			if(file.exists()) 
+			{ 
+				FileWriter fw=new FileWriter(file,true);
+				System.out.println("Enter contents to write in File:");
+				String writeString=scanWrite.nextLine();
+				PrintWriter pw=new PrintWriter(fw);
+				pw.println(writeString);
+				pw.close();
+				System.out.println("Successfully written."); 
+			} 
+			else
+			{
+				System.out.println("No such File exists!"); 
+			}
+		}
+	}
+	
+	public void readFromFile() throws IOException
+	{
+		Scanner scanRead=new Scanner(System.in);
+		System.out.println("Enter the path from where you want to read from a File:");
+		String dirPath=scanRead.nextLine();
+		File dir=new File(dirPath);
+		if(!dir.exists())
+		{
+			System.out.println("No such Directory exists!");
+			return;
+		}
+		else
+		{
+			System.out.println("Enter name of File to read from:");
+			String fileName=scanRead.nextLine();
+			File file=new File(dirPath,fileName);
+			if(file.exists())
+			{
+				System.out.println("File contains:");
+				int ch;
+				FileReader fr=new FileReader(file);
+				while((ch=fr.read())!=-1)
+				{
+					System.out.print((char)ch);
+				}
+				fr.close();
+				System.out.println();
+			}
+			else
+			{
+				System.out.println("No such File exists!");
+			}
+		}
+	}
+	
+	public void deleteFile()
+	{
+		Scanner scanDel=new Scanner(System.in);
+		System.out.println("Enter the path from where you want to delete a File:");
+		String dirPath=scanDel.nextLine();
+		File dir=new File(dirPath);
+		if(!dir.exists())
+		{
+			System.out.println("No such Directory exists!");
+			return;
+		}
+		else
+		{
+			System.out.println("Enter name of File to be deleted:");
+			String fileName=scanDel.nextLine();
+			File file=new File(dirPath,fileName);
+			if(file.exists())
+			{
+				if(file.delete())
+				{
+					System.out.println("File has been deleted");
+				}
+				else
+				{
+					System.out.println("Could not delete file");
+				}
+			}
+			else
+			{
+				System.out.println("No such File exists!");
+			}
+		}
+	}
+	
+	public void searchFile()
+	{
+		Scanner scanSear=new Scanner(System.in); 
+		System.out.println("Enter path of Directory:");
+		String dirPath=scanSear.nextLine();
+		File dir=new File(dirPath);
+		if(!dir.exists())
+		{
+			System.out.println("No such Directory exists!");
+			return;
+		}
+		else
+		{	
+			File[] filesList=dir.listFiles();
+			if(filesList.length!=0)
+			{
+				System.out.println("Enter name of File to search:");
+				String fileName=scanSear.nextLine();
+				for(int i=0;i<filesList.length;i++)
+				{
+					if(filesList[i].getName().equals(fileName))
+					{
+						System.out.println("File found at given path!");
+						return;
+					}
+				}
+				System.out.println("No such File found");
+			}
+			else
+				System.out.println("No Files present in path:"+dirPath);
+		}
+	}
+	
 }
